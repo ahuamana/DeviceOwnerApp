37acf1a352ba494c75f9d413804fc170a2bc572b
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2025-05-22 16:19:41 -0500

    add readme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.ahuaman.deviceadminoculustest"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.ahuaman.deviceadminoculustest"
